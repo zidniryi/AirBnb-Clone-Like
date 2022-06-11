05f3b62a41e9fc5d7a9a94fbdb8e28f574cf9b7c
zidniryi <zidnirydwan@gmail.com>
2022-06-12 00:48:02 +0700

    add service

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,26 +8,11 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Array<Recipe> = [
-    {
-      id: 'r1',
-      title: 'Saffron Rice',
-      imageUrl:
-        // eslint-disable-next-line max-len
-        'https://asset.kompas.com/crops/fQVOtLSPe9n2ymE8jOG6fNCf0xQ=/0x0:1000x667/750x500/data/photo/2021/10/21/6170ffb140316.jpg',
-      ingridients: ['rice', 'saffron', 'chicken'],
-    },
-    {
-      id: 'r2',
-      title: 'Spaghetti',
-      imageUrl:
-        // eslint-disable-next-line max-len
-        'https://cdn.medcom.id/dynamic/content/2016/08/20/571823/ST3eIAmLz5.jpg?w=480',
-      ingridients: ['mie', 'saffron', 'chicken'],
-    },
-  ];
+  recipes: Array<Recipe>;
 
-  constructor() {}
+  constructor(private recipeServices: RecipesService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipes = this.recipeServices.getAllRecipes();
+  }
 }
